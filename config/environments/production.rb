7a0f76cd8c4f449390f866cc85d9ce42967a7a1f
Kiruthikram Prabhakar <kiruthikramprabhakar@gmail.com>
2023-04-24 10:49:06 +0530

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
 
 
 
-  config.hosts << "localhost:3000"
+  config.hosts << "movie-review-app-bfus.onrender.com"
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
